c02cbaa74813e5a98cfa416ceed26def851cb03e
ian.malm <ian.malm@AN0830221077155.dinfor.uniceub>
2018-05-04 19:24:11 -0300

    Residencia DAO e MB

---
+++ src/java/mb/ResidenciaMB.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author ian.malm
+ */
+@ManagedBean
+@ViewScoped
+public class ResidenciaMB {
+
+    /**
+     * Creates a new instance of ResidenciaMB
+     */
+    public ResidenciaMB() {
+    }
+    
+}
